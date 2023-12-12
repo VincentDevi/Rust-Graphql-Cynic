ea7e16a59c78b2f8be45226406ee31212a9b01c8
Devilers Vincent <vincent.devilers@gmail.com>
2023-12-12 10:18:08 +0100

    fetching data

--- src/pages/home/mod.rs
+++ src/pages/home/mod.rs
@@ -1,8 +1,17 @@
-use cynic::QueryBuilder;
+use crate::pages::home::query::MyQuery;
+use cynic::{GraphQlResponse, QueryBuilder};
 use leptos::*;
 
 mod query;
 
+pub async fn my_query() {
+    let operation = MyQuery::build(());
+    let res = reqwest::Client::new();
+    let response_body = res.get("").json(&operation).send()?;
+
+    let result = response_body.json::<GraphQlResponse<MyQuery>>()?;
+    result
+}
 #[component]
 pub fn HomePage() -> impl IntoView {
     let (ok, set_ok) = create_signal(0);
