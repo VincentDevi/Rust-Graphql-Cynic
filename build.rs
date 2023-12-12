d234b110a84f9bbb47c17f09757f2d74dab22173
Devilers Vincent <vincent.devilers@gmail.com>
2023-12-12 09:59:56 +0100

    new schema

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
-  cynic_codegen::register_schema("starwars")
-    .from_sdl_file("schema.graphql")
-    .unwrap()
-    .as_default()
-    .unwrap();
+    cynic_codegen::register_schema("schema")
+        .from_sdl_file("schema.graphql")
+        .unwrap()
+        .as_default()
+        .unwrap();
 }
